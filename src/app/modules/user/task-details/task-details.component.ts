99295664854909958fb14d73ec3200031598d68e
Bhavani <bhavani@itprofound.com>
2021-01-19 18:13:20 +0530

    ui sheets

--- src/app/modules/user/task-details/task-details.component.ts
+++ src/app/modules/user/task-details/task-details.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { CalendarOptions } from '@fullcalendar/angular';
+import { Subject } from 'rxjs';
+import { addDays, getHours } from 'date-fns';
+import {
+  CalendarEvent,
+  CalendarEventTimesChangedEvent,
+  CalendarView,
+} from 'angular-calendar';
+import { colors } from '../calendar-utils/colors';
 
 import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-task-details',
-  template: '<ejs-schedule></ejs-schedule>',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './task-details.component.html',
   styleUrls: ['./task-details.component.css']
 })
@@ -14,30 +21,62 @@ export class TaskDetailsComponent implements OnInit {
   taskListArray: any[];
   noData: boolean = false;
 
-  calendarOptions: CalendarOptions = {
-    initialView: 'dayGridWeek',
-    // headerToolbar: {
-    //   left: 'prev, next today',
-    //   center: 'title',
-    //   right: 'dayGridMonth, dayGridWeek, listWeek'
-    // },
-    dayMaxEvents: true,
-    events: [
-      { title: 'event 1', date: '2021-01-17' },
-    ]
-  };
+  view: CalendarView = CalendarView.Week;
+
+  viewDate: Date = new Date();
+
+  dayStartHour = Math.max(0, getHours(new Date()) - 2);
+
+  dayEndHour = Math.min(23, getHours(new Date()) + 2);
+
+  events: CalendarEvent[] = [
+    {
+      title: '9hrs',
+      color: colors.yellow,
+      start: new Date(),
+      end: addDays(new Date(), 1), // an end date is always required for resizable events to work
+      resizable: {
+        beforeStart: true, // this allows you to configure the sides the event is resizable from
+        afterEnd: true,
+      },
+    },
+    {
+      title: '8hrs',
+      color: colors.blue,
+      start: new Date(),
+      end: addDays(new Date(), 1),
+      resizable: {
+        beforeStart: true, // this allows you to configure the sides the event is resizable from
+        afterEnd: true,
+      },
+    },
+  ];
+
+  refresh: Subject<any> = new Subject();
+
+  eventTimesChanged({
+    event,
+    newStart,
+    newEnd,
+  }: CalendarEventTimesChangedEvent): void {
+    event.start = newStart;
+    event.end = newEnd;
+    this.refresh.next();
+  }
 
   constructor(private taskService: TaskService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.dataState();
     let t = this.taskService.getTaskList();
+    console.log(t)
     t.snapshotChanges().subscribe(data => {
       this.taskListArray = [];
       data.forEach(item => {
         let a = item.payload.toJSON();
         a['key'] = item.key;
         this.taskListArray.push(a);
+        // console.log(this.taskListArray)
       })
     })
   }
