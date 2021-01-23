7545b875659f8b615532e9e29d78f413be74825b
Bhavani <bhavani@itprofound.com>
2021-01-23 14:36:45 +0530

    nothing

--- src/app/modules/user/apply-leave/apply-leave.component.ts
+++ src/app/modules/user/apply-leave/apply-leave.component.ts
@@ -31,21 +31,21 @@ export class ApplyLeaveComponent implements OnInit, AfterViewInit {
     // console.log(this.appCalendar)
   }
 
-  getDaysInMonth(month: number, year: number) {
-    var date = new Date(year, month, 1);
-    // var previousDate = new Date(year, month + 1, 1);
-    var days = [];
-    while (date.getMonth() === month) {
-      days.push(new Date(date));
-      date.setDate(date.getDate() + 1);
-    }
+  // getDaysInMonth(month: number, year: number) {
+  //   var date = new Date(year, month, 1);
+  //   // var previousDate = new Date(year, month + 1, 1);
+  //   var days = [];
+  //   while (date.getMonth() === month) {
+  //     days.push(new Date(date));
+  //     date.setDate(date.getDate() + 1);
+  //   }
     // while (previousDate.getMonth() === month + 1) {
     //   days.push(new Date(previousDate));
     //   date.setDate(previousDate.getDate() + 1); 
     // }
-    console.log(days) ;
+    // console.log(days) ;
 
   }
 
 
-}
+// }
