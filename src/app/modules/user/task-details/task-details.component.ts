4a1370150f0453f4c5243123264f671c8cd730c6
bhavani <bhavani@itprofound.com>
2021-02-03 18:33:52 +0530

    task forms

--- src/app/modules/user/task-details/task-details.component.ts
+++ src/app/modules/user/task-details/task-details.component.ts
@@ -4,7 +4,7 @@ import { TaskService } from 'src/app/services/task.service';
 import { AddProjectComponent } from '../modals/add-project/add-project.component';
 import { AddTaskComponent } from '../modals/add-task/add-task.component';
 
-
+import { FormGroup, FormArray, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-task-details',
@@ -15,14 +15,44 @@ export class TaskDetailsComponent implements OnInit {
   modalRef: BsModalRef | null;
   modalRef2: BsModalRef;
   projectList:any;
-  constructor(private modalService: BsModalService, private taskService:TaskService) {
+  public taskForm: FormGroup;
+  submitted = false;
+
+  constructor(private modalService: BsModalService, private taskService:TaskService, private fb: FormBuilder) {
 
   }
 
   ngOnInit() {
     this.displayDetails();
+    this.taskForm = this.fb.group({
+      taskName: ['', Validators.required],
+      taskCode: ['', Validators.required],
+      status: ['', Validators.required],
+      columns: this.fb.array([this.initDuration()])
+    });
   }
 
+  get formArr() {
+    return this.taskForm.get("columns") as FormArray;
+  }
+
+  onSave() {
+    this.submitted = true;
+    if(!this.taskForm.valid) {
+      return;
+    }
+    console.log(JSON.stringify(this.taskForm.value))
+  }
+
+  initDuration() {
+    return this.fb.group({
+      duration: [""]
+    });
+  }
+
+  addNewCoulmn() {
+    this.formArr.push(this.initDuration());
+  }
  
   onAddTask() {
     this.modalRef = this.modalService.show(AddTaskComponent, { class: 'modal-lg' });
