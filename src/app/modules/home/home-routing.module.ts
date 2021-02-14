66b5e38a2a40cb2f228741164c392bb50f7d300b
BhavaniAnu <email>
2021-02-14 23:14:35 +0530

    calendar companyinfo

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthguardGuard } from "src/app/authguard.guard";
+import { CompanyInfoComponent } from "./company-info/company-info.component";
 import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-    { path: '', component: HomeComponent , canActivate: [AuthguardGuard] }
+    { path: '', component: HomeComponent , canActivate: [AuthguardGuard] },
+    { path: 'companyInfo', component: CompanyInfoComponent }
 ];
 
 @NgModule({
