48cbb9a6dfe0615c28d5493d76a9735607f44acc
bhavani <bhavani@itprofound.com>
2021-02-16 14:54:04 +0530

    leave type

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from "@angular/common";
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { SharedModule } from "../shared/shared.module";
 import { CoreModule } from "../core/core.module";
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { DashboardRoutingModule } from "./dashboard-routing.module";
 
 
@@ -20,9 +20,8 @@ import { LeavetypeDetailsComponent } from "./leavetype-contents/leavetype-detail
 import { LeavetypeListComponent } from "./leavetype-contents/leavetype-list/leavetype-list.component";
 import { LeavetypeMainComponent } from "./leavetype-contents/leavetype-main/leavetype-main.component";
 import { LeavetypeManageComponent } from "./leavetype-contents/leavetype-manage/leavetype-manage.component";
-import { MaterialModule } from "../shared/material/materail.module";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import {NgxPaginationModule} from 'ngx-pagination';
+import { MaterialModule } from "../shared/material/material.module";
 
 @NgModule({
     declarations: [
