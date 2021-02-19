1749c965db4c32518110a73b7f2f97da09ca6fb3
bhavani <bhavani@itprofound.com>
2021-02-19 09:31:28 +0530

    changes

--- src/app/modules/dashboard/leaverequest-contents/leaverequest-list/leaverequest-list.component.ts
+++ src/app/modules/dashboard/leaverequest-contents/leaverequest-list/leaverequest-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { EmployeeService } from 'src/app/services/employee.service';
+import { LeaveService } from 'src/app/services/leave.service';
 
 @Component({
   selector: 'app-leaverequest-list',
@@ -6,10 +9,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leaverequest-list.component.css']
 })
 export class LeaverequestListComponent implements OnInit {
+  leaveRequests;
+  errorMsg;
 
-  constructor() { }
+  loading = true;
+  currentPage = 1;
+  totalElements;
+  numberOfElements;
+  size = 10;
+  sortKey = 'fromDate';
+  reverse = false;
+
+  allEmployees: Observable<any>;
+  employeeinput$ = new Subject<string>();
+  isSelectLoading = false;
+
+  constructor(private empLeaveService: LeaveService, private empService: EmployeeService) { }
 
   ngOnInit() {
+    this.getAllEmployeeLeaves();
+    // this.loadEmployee();
   }
+
+  getPage(page: number) {
+    this.loading = true;
+    this.currentPage = page;
+    this.getAllEmployeeLeaves();
+  }
+
+  sort(key: string) {
+    this.loading = true;
+    this.sortKey = key + ','.concat(this.reverse ? 'DESC' : 'ASC');
+    this.reverse = !this.reverse;
+    this.getAllEmployeeLeaves();
+  }
+
+  getAllEmployeeLeaves() {
+    this.empLeaveService.getAllEmployeeLeaves().subscribe(
+      data => {
+        this.leaveRequests = data;
+        this.leaveRequests = data.data;
+        console.log(this.leaveRequests)
+        this.totalElements = data.totalElements;
+        // this.size = data.size;
+        this.numberOfElements = data.numberOfElements;
+        this.loading = false;
+      },
+      error => this.errorMsg = error
+    );
+  }
+
+  // private loadEmployee() {
+  //   this.allEmployees = concat(
+  //     of([]), // default items
+  //     this.employeeinput$.pipe(
+  //       debounceTime(200),
+  //       distinctUntilChanged(),
+  //       tap(() => this.isSelectLoading = true),
+  //       switchMap(term => this.empService.getEmployeeByFullName(term).pipe(
+  //         catchError(() => of([])), // empty list on error
+  //         tap(() => this.isSelectLoading = false)
+  //       ))
+  //     )
+  //   );
+  // }
+
 
 }
