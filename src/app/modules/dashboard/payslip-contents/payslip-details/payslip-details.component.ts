1d2487edfc99d6e7e75968b9cba58663d53babf6
bhavani <bhavani@itprofound.com>
2021-03-01 18:25:31 +0530

    payslip list

--- src/app/modules/dashboard/payslip-contents/payslip-details/payslip-details.component.ts
+++ src/app/modules/dashboard/payslip-contents/payslip-details/payslip-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { PaySlipService } from 'src/app/services/payslip.service';
 
 @Component({
   selector: 'app-payslip-details',
@@ -6,10 +9,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payslip-details.component.css']
 })
 export class PayslipDetailsComponent implements OnInit {
+  private id: number;
+  private sub: any;
+  selectedPay: any;
+  isPayslipSelected= false;
 
-  constructor() { }
+  minDate;
+  isEdit = false;
+  errorMsg;
+  pay_update_msg;
+  has_error = false;
+  submitted = false;
+  payUpdateForm: FormGroup;
+
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, 
+    private payslipService: PaySlipService) { }
 
   ngOnInit() {
+    this.routeId();
   }
+
+  routeId() {
+    this.sub = this.route.params.subscribe(params => {
+      this.id = params['id']; // (+) converts string 'id' to a number
+      this.getPayslipById(this.id);
+    });
+  }
+
+  initPayUpdateForm() {
+    this.minDate = new Date();
+    this.payUpdateForm = this.formBuilder.group({
+      projectCode: [this.selectedPay.projectCode],      
+      projectName: [this.selectedPay.projectName, [Validators.required, Validators.minLength(3)]],
+      startDate: [this.selectedPay.startDate],
+      endDate: [this.selectedPay.endDate]
+    });
+  }
+
+  toggleEdit() {
+    this.isEdit = !this.isEdit;
+    if (this.isEdit) {
+      this.initPayUpdateForm();
+    }
+  }
+
+  getPayslipById(id) {
+    if (id ) {
+      this.payslipService.getPayslipById(id)
+        .subscribe(
+          data => {
+            console.log(data)
+            this.selectedPay = data;
+            this.selectedPay=this.selectedPay.result
+            this.isPayslipSelected = true;
+            // console.log("Selected Leave Type data: ", data);
+          },
+          error => this.errorMsg = error);
+    } else {
+      this.isPayslipSelected = false;
+    }
+  }
+
+  get f() { return this.payUpdateForm.controls; }
 
 }
